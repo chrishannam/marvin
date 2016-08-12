ab250d8113a868fed4febd2ac52b795342889e2d
Chris Hannam <bassdread@gmail.com>
2016-08-12 11:16:35 +0100

    improve channel review

--- historical/channel_review.py
+++ historical/channel_review.py
@@ -1,7 +1,20 @@
+""" A small program that accesses the history of provided Slack channels
+and sends the text to Algorithmia to perform sentiment analysis.
+
+This analysis is the displayed giving the average number of the following:
+
+Very negative
+Negative
+Neutral
+Positive
+Very positive
+"""
+
+
 import Algorithmia
 import yaml
 import json
-
+import argparse
 from slackclient import SlackClient
 from datetime import datetime, date, time, timedelta
 
@@ -10,41 +23,60 @@ CONFIG = yaml.load(file('../python-rtmbot-master/rtmbot.conf', 'r'))
 ALGORITHMIA_CLIENT = Algorithmia.client(CONFIG["ALGORITHMIA_KEY"])
 ALGORITHM = ALGORITHMIA_CLIENT.algo('nlp/SentimentAnalysis/0.1.2')
 
-print CONFIG
+def list_channels(slack_client):
+    channel_list_raw = slack_client.api_call("channels.list", exclude_archived=1)
 
-token = CONFIG['SLACK_TOKEN'] # found at https://api.slack.com/web#authentication
-sc = SlackClient(token)
+    channels = {}
 
-midnight = datetime.combine(date.today(), time.min)
-yesterday_midnight = midnight - timedelta(days=1)
+    for channel in channel_list['channels']:
 
-channel = 'C03T49SPB'
+        channel_id = channel.get("id", "ID not found")
+        channel_name = channel.get("name", "Name not found")
 
-history = sc.api_call("channels.history", channel='C03T49SPB', inclusive=1,count=50)
+        print "Name: {} ID: {}".format(channel_name, channel_id)
+        channels[channel_name] = channel_id
 
-history = json.loads(history)
+    return channels
 
-MAPPING = {
-    0: 'very negative',
-    1: 'negative',
-    2: 'neutral',
-    3: 'positive',
-    4: 'very positive'
-}
+def run():
 
-sentiment_results = {
-    'very negative': 0,
-    'negative': 0,
-    'neutral': 0,
-    'positive': 0,
-    'very positive': 0,
-}
+    token = CONFIG['SLACK_TOKEN'] # found at https://api.slack.com/web#authentication
+    sc = SlackClient(token)
 
-for message in history['messages']:
-    results = ALGORITHM.pipe(message['text'])
+    midnight = datetime.combine(date.today(), time.min)
+    yesterday_midnight = midnight - timedelta(days=1)
 
-    if not results.result:
-        continue
-    sentiment_results[MAPPING[results.result]] += 1
+    channel = 'C03T49SPB'
 
-print sentiment_results
+    history = sc.api_call("channels.history", channel='C03T49SPB', inclusive=1,count=50)
+
+    history = json.loads(history)
+
+    MAPPING = {
+        0: 'very negative',
+        1: 'negative',
+        2: 'neutral',
+        3: 'positive',
+        4: 'very positive'
+    }
+
+    sentiment_results = {
+        'very negative': 0,
+        'negative': 0,
+        'neutral': 0,
+        'positive': 0,
+        'very positive': 0,
+    }
+
+    for message in history['messages']:
+        results = ALGORITHM.pipe(message['text'])
+
+        if not results.result:
+            continue
+        sentiment_results[MAPPING[results.result]] += 1
+
+    print sentiment_results
+
+
+if __name__ == "__main__":
+    run()
--- python-rtmbot-master/plugins/marvin_the_sentiment_analyser.py
+++ python-rtmbot-master/plugins/marvin_the_sentiment_analyser.py
@@ -1,39 +1,39 @@
 import Algorithmia
 import yaml
 
-CONFIG = yaml.load(file('rtmbot.conf', 'r'))
+CONFIG = yaml.load(file("rtmbot.conf", "r"))
 
 ALGORITHMIA_CLIENT = Algorithmia.client(CONFIG["ALGORITHMIA_KEY"])
-ALGORITHM = ALGORITHMIA_CLIENT.algo('nlp/SentimentAnalysis/0.1.2')
+ALGORITHM = ALGORITHMIA_CLIENT.algo("nlp/SentimentAnalysis/0.1.2")
 
 outputs = []
 
 MAPPING = {
-    0: 'very negative',
-    1: 'negative',
-    2: 'neutral',
-    3: 'positive',
-    4: 'very positive'
+    0: "very negative",
+    1: "negative",
+    2: "neutral",
+    3: "positive",
+    4: "very positive"
 }
 
 sentiment_results = {
-    'very negative': 0,
-    'negative': 0,
-    'neutral': 0,
-    'positive': 0,
-    'very positive': 0,
+    "very negative": 0,
+    "negative": 0,
+    "neutral": 0,
+    "positive": 0,
+    "very positive": 0,
 }
 
 
 def process_message(data):
 
-    text = data.get('text', None)
+    text = data.get("text", None)
 
-    if not text or data.get('subtype', '') == 'channel_join':
+    if not text or data.get("subtype", "") == "channel_join":
         return
 
-    if 'what's the mood?' in text:
-        outputs.append([data['channel'], str(sentiment_results)])
+    if "what's the mood?" in text:
+        outputs.append([data["channel"], str(sentiment_results)])
         return
 
     results = ALGORITHM.pipe(text)
@@ -41,6 +41,6 @@ def process_message(data):
     sentiment_results[MAPPING[results.result]] += 1
 
     if results.result == 0:
-        outputs.append([data['channel'], "Easy there, negative Nancy!"])
+        outputs.append([data["channel"], "Easy there, negative Nancy!"])
 
     print "Comment was {}".format(MAPPING[results.result])
